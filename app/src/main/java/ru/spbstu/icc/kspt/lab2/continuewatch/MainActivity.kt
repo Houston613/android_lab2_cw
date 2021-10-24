6323b8656ae46784a7dfc25dde380a93c1e3a588
Anton Chervak <mr.antonchervak@gmail.com>
2021-10-25 02:56:55 +0300

    исправления лаб 2

--- app/src/main/java/ru/spbstu/icc/kspt/lab2/continuewatch/MainActivity.kt
+++ app/src/main/java/ru/spbstu/icc/kspt/lab2/continuewatch/MainActivity.kt
@@ -3,61 +3,57 @@ package ru.spbstu.icc.kspt.lab2.continuewatch
 import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
-    var secondsElapsed: Int = 0
-    var seconds: Int = 0
-    val tag = "MainActivity"
-    lateinit var textSecondsElapsed: TextView
-
-
-    var backgroundThread = Thread {
+    private var secondsElapsed: Int = 0
+    private var seconds: Int = 0
+    private val tag = "MainActivity"
+    private lateinit var textSecondsElapsed: TextView
+    private var backgroundThread = Thread {
         while (true) {
             Thread.sleep(1000)
             textSecondsElapsed.post {
-                textSecondsElapsed.setText("Seconds elapsed: " + secondsElapsed++)
+                textSecondsElapsed.text = getString(R.string.second_elapsed, secondsElapsed++)
             }
         }
     }
-    lateinit var sharedPreferences: SharedPreferences
-    /*(override fun onSaveInstanceState(outState: Bundle) {
+    private lateinit var sharedPreferences: SharedPreferences
+
+    /* override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putInt("timeStop",secondsElapsed)
+        outState.run {putInt("timeStop",secondsElapsed)}
         Log.i(tag,"MainActivity InstanceSaved")
     }
-
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
-        secondsElapsed = savedInstanceState.getInt("time")
+        savedInstanceState.run { secondsElapsed = getInt("timeStop")}
         Log.i(tag,"MainActivity InstanceRestore")
     }
 
-
-     */
+         */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         textSecondsElapsed = findViewById(R.id.textSecondsElapsed)
         backgroundThread.start()
-        sharedPreferences = getSharedPreferences("Main",Context.MODE_PRIVATE)
+        sharedPreferences = getSharedPreferences("Main", Context.MODE_PRIVATE)
         Log.i(tag, "MainActivity: created")
     }
+
     override fun onStop() {
         super.onStop()
         with(sharedPreferences.edit()){
             putInt("timeStop",secondsElapsed)
             apply()
         }
-        Log.i(tag, "MainActivity: created")
+        Log.i(tag, "MainActivity: stopped")
     }
     override fun onStart() {
         super.onStart()
-        secondsElapsed = seconds
+        //secondsElapsed = seconds
         secondsElapsed = sharedPreferences.getInt("timeStop",0)
         with(sharedPreferences.edit()){
             remove("timeStop")
@@ -65,14 +61,20 @@ class MainActivity : AppCompatActivity() {
         Log.i(tag, "MainActivity: started")
     }
 
+
     override fun onPause() {
         super.onPause()
-        //seconds = secondsElapsed
         Log.i(tag, "MainActivity: Paused")
 
     }
     override fun onResume() {
         super.onResume()
         Log.i(tag, "MainActivity: resumed")
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(tag, "MainActivity: destroyed")
     }
 }
